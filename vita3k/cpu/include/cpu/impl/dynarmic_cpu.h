e90839973fed67b2d3944dedbe79008daca7ffa5
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-03-25 22:53:47 +0800

    Update dynarmic_cpu.h

--- vita3k/cpu/include/cpu/impl/dynarmic_cpu.h
+++ vita3k/cpu/include/cpu/impl/dynarmic_cpu.h
@@ -87,7 +87,7 @@ public:
     void set_fpscr(uint32_t val) override;
 
     CPUContext save_context() override;
-    void load_context(CPUContext context) override;
+    void load_context(const CPUContext &ctx) override;
 
     bool is_thumb_mode() override;
     int step() override;
