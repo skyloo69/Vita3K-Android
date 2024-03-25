9e54c30360c634ca6d302c6947b67099dae8af37
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-03-25 21:35:05 +0800

    Update dynarmic_cpu.h

--- vita3k/cpu/include/cpu/impl/dynarmic_cpu.h
+++ vita3k/cpu/include/cpu/impl/dynarmic_cpu.h
@@ -18,17 +18,14 @@
 #pragma once
 
 #include <dynarmic/interface/A32/a32.h>
-#include <dynarmic/interface/A32/context.h>
 #include <dynarmic/interface/A32/coprocessor.h>
 #include <dynarmic/interface/exclusive_monitor.h>
 
 #include <cpu/functions.h>
-#include <cpu/impl/interface.h>
 #ifdef USE_UNICORN
 #include <cpu/impl/unicorn_cpu.h>
 #endif
 
-#include <functional>
 #include <memory>
 
 class ArmDynarmicCallback;
@@ -87,7 +84,7 @@ public:
     void set_fpscr(uint32_t val) override;
 
     CPUContext save_context() override;
-    void load_context(CPUContext context) override;
+    void load_context(const CPUContext &ctx) override;
 
     bool is_thumb_mode() override;
     int step() override;
