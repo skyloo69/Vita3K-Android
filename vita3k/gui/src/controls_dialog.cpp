2ea6435bf0f7523c266f7e4f45d5e7f76accb632
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-04-05 22:45:27 +0800

    Update controls_dialog.cpp

--- vita3k/gui/src/controls_dialog.cpp
+++ vita3k/gui/src/controls_dialog.cpp
@@ -17,7 +17,6 @@
 
 #include "private.h"
 
-#include <SDL_scancode.h>
 #include <config/functions.h>
 #include <config/state.h>
 #include <emuenv/state.h>
@@ -121,6 +120,9 @@ void draw_controls_dialog(GuiState &gui, EmuEnvState &emuenv) {
 
     const ImVec2 display_size(emuenv.viewport_size.x, emuenv.viewport_size.y);
     const auto RES_SCALE = ImVec2(display_size.x / emuenv.res_width_dpi_scale, display_size.y / emuenv.res_height_dpi_scale);
+    static const auto BUTTON_SIZE = ImVec2(120.f * emuenv.dpi_scale, 0.f);
+
+    const auto RES_SCALE = ImVec2(display_size.x / emuenv.res_width_dpi_scale, display_size.y / emuenv.res_height_dpi_scale);
     ImGui::SetNextWindowPos(ImVec2(display_size.x / 2.f, display_size.y / 2.f), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
     ImGui::Begin("Overlay", &gui.controls_menu.controls_dialog, ImGuiWindowFlags_AlwaysAutoResize);
     ImGui::SetWindowFontScale(RES_SCALE.x);
@@ -199,14 +201,6 @@ static char const *SDL_key_to_string[]{ "[unset]", "[unknown]", "[unknown]", "[u
 
 static const short total_key_entries = 28;
 
-static bool exists_in_array(int *ptr, int val, size_t size) {
-    if (!ptr || !size) {
-        return false;
-    }
-    int *end = ptr + size;
-    return std::find(ptr, end, val) != end;
-}
-
 static void prepare_map_array(EmuEnvState &emuenv, std::array<int, total_key_entries> &map) {
     map[0] = emuenv.cfg.keyboard_leftstick_up;
     map[1] = emuenv.cfg.keyboard_leftstick_down;
@@ -262,7 +256,7 @@ static void remapper_button(GuiState &gui, EmuEnvState &emuenv, int *button, con
             *button = gui.captured_key;
             if (*button < 0 || *button > 231)
                 *button = 0;
-            else if (gui.is_key_capture_dropped || (!gui.is_capturing_keys && *button != key_association && exists_in_array(original_state.data(), *button, original_state.size()))) {
+            else if (gui.is_key_capture_dropped || (!gui.is_capturing_keys && *button != key_association && vector_utils::contains(original_state, *button))) {
                 // undo the changes
                 *button = key_association;
                 gui.is_key_capture_dropped = false;
@@ -282,9 +276,21 @@ void draw_controls_dialog(GuiState &gui, EmuEnvState &emuenv) {
         ImGui::EndMainMenuBar();
     }
 
+    auto &lang = gui.lang.controls;
+    auto &common = emuenv.common_dialog.lang.common;
+    
     ImGui::SetNextWindowSize(ImVec2(0, height));
     ImGui::SetNextWindowPos(ImVec2(ImGui::GetIO().DisplaySize.x / 2.f, ImGui::GetIO().DisplaySize.y / 2.f), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
-    ImGui::Begin(lang["title"].c_str(), &gui.controls_menu.controls_dialog);
+    
+    ImGui::Begin("##controls", &gui.controls_menu.controls_dialog, ImGuiWindowFlags_NoTitleBar);
+    ImGui::SetWindowFontScale(RES_SCALE.x);
+    auto title_str = lang["title"].c_str();
+    ImGui::SetCursorPosX((ImGui::GetWindowSize().x / 2.f) - (ImGui::CalcTextSize(title_str).x / 2.f));
+    ImGui::TextColored(GUI_COLOR_TEXT_TITLE, "%s", title_str);
+    ImGui::Spacing();
+    ImGui::Separator();
+
+
     if (ImGui::BeginTable("main", 2)) {
         ImGui::TableSetupColumn("button");
         ImGui::TableSetupColumn("mapped_button");
@@ -361,15 +367,22 @@ void draw_controls_dialog(GuiState &gui, EmuEnvState &emuenv) {
     ImGui::SetNextWindowPos(ImVec2(ImGui::GetIO().DisplaySize.x / 2.f, ImGui::GetIO().DisplaySize.y / 2.f), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
     if (ImGui::BeginPopupModal(lang["error"].c_str(), nullptr, ImGuiWindowFlags_AlwaysAutoResize)) {
         ImGui::Text("%s", lang["error_duplicate_key"].c_str());
-        ImGui::NewLine();
-        static const auto BUTTON_SIZE = ImVec2(120.f * emuenv.dpi_scale, 0.f);
+        ImGui::Spacing();
+        ImGui::Separator();
+        ImGui::Spacing();
         ImGui::SetCursorPosX((ImGui::GetWindowSize().x / 2.f) - (BUTTON_SIZE.x / 2.f));
-        if (ImGui::Button(emuenv.common_dialog.lang.common["ok"].c_str(), BUTTON_SIZE)) {
+        if (ImGui::Button(common["ok"].c_str(), BUTTON_SIZE))
             ImGui::CloseCurrentPopup();
-        }
+
         ImGui::EndPopup();
     }
-
+    ImGui::Spacing();
+    ImGui::Separator();
+    ImGui::Spacing();
+    ImGui::SetCursorPosX((ImGui::GetWindowSize().x / 2.f) - (BUTTON_SIZE.x / 2.f));
+    if (ImGui::Button(common["close"].c_str(), BUTTON_SIZE))
+        gui.controls_menu.controls_dialog = false;
+    
     ImGui::End();
 }
 
