db79780f860bbc6546603f197532f6b800e37771
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-05-18 21:04:34 +0800

    Update HIDDeviceManager.java

--- android/src/main/java/org/libsdl/app/HIDDeviceManager.java
+++ android/src/main/java/org/libsdl/app/HIDDeviceManager.java
@@ -705,7 +705,7 @@ public class HIDDeviceManager {
     private native void HIDDeviceRegisterCallback();
     private native void HIDDeviceReleaseCallback();
 
-    native void HIDDeviceConnected(int deviceID, String identifier, int vendorId, int productId, String serial_number, int release_number, String manufacturer_string, String product_string, int interface_number, int interface_class, int interface_subclass, int interface_protocol);
+    native void HIDDeviceConnected(int deviceID, String identifier, int vendorId, int productId, String serial_number, int release_number, String manufacturer_string, String product_string, int interface_number);
     native void HIDDeviceOpenPending(int deviceID);
     native void HIDDeviceOpenResult(int deviceID, boolean opened);
     native void HIDDeviceDisconnected(int deviceID);
