4f75b6f7370f59479624a6b535d2527595be24fa
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-05-30 14:27:51 +0800

    revert gui.cpp

--- vita3k/gui/src/gui.cpp
+++ vita3k/gui/src/gui.cpp
@@ -266,7 +266,7 @@ static void init_font(GuiState &gui, EmuEnvState &emuenv) {
 
             const auto sys_lang = static_cast<SceSystemParamLang>(emuenv.cfg.sys_lang);
             if (sys_lang == SCE_SYSTEM_PARAM_LANG_CHINESE_S) {
-                const std::vector<uint8_t> font_source = fs_utils::read_asset_raw(default_font_path / "NotoSerifCJK-Regular.ttc");
+                const std::vector<uint8_t> font_source = fs_utils::read_asset_raw(default_font_path / "SourceHanSansSC-Bold-Min.ttf");
                 
                 if (!font_source.empty()) {
                     font_data = malloc(font_source.size());
