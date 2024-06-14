816b08a0a1a88b6898028b6ec328c222975ee96a
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-06-14 15:52:59 +0800

    Update private.h

--- vita3k/gui/src/private.h
+++ vita3k/gui/src/private.h
@@ -76,6 +76,7 @@ void draw_trophy_collection(GuiState &gui, EmuEnvState &emuenv);
 void draw_user_management(GuiState &gui, EmuEnvState &emuenv);
 
 void reevaluate_code(GuiState &gui, EmuEnvState &emuenv);
+
 void SetTooltipEx(const char *tooltip);
 
 } // namespace gui
