e60a3df909a8b68693cc9caea09247fa80b77c1d
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-07-01 21:25:02 +0800

    Update state.h

--- vita3k/lang/include/lang/state.h
+++ vita3k/lang/include/lang/state.h
@@ -649,7 +649,7 @@ struct LangState {
             { "sensor_enable", "Enable HW acceleration and gyroscope" },
             { "sensor_emu_pos", "Emulate acceleration position sensor" },
             { "sensors_description", "Enable built in sensor or disable sensor and lock specific sensor position" },
-            { "screenmode_pos", "Change Screen position" },
+            { "screenmode_pos", "Change Screen position (restart app to take effect)" },
             { "screenmode_auto", "Auto landscapes" },
             { "screenmode_Left", "Lock to landscapes left" },
             { "screenmode_right", "Lock to Landscapes right" },
