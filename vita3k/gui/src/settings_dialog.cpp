083689f99123feeb0135b627d5eb65a99433b8fd
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-07-20 23:20:39 +0800

    Update settings_dialog.cpp

--- vita3k/gui/src/settings_dialog.cpp
+++ vita3k/gui/src/settings_dialog.cpp
@@ -1131,7 +1131,7 @@ void draw_settings_dialog(GuiState &gui, EmuEnvState &emuenv) {
         ImGui::SetCursorPosX((ImGui::GetWindowWidth() / 2.f) - (ImGui::CalcTextSize(lang.emulator["sensor_settings"].c_str()).x / 2.f));
         ImGui::TextColored(GUI_COLOR_TEXT_TITLE, "%s", lang.emulator["sensor_settings"].c_str());
         ImGui::Spacing();
-        if (ImGui::Checkbox(lang.emulator["sensor_enable"].c_str(), &emuenv.cfg.tiltsens)){}
+        ImGui::Checkbox(lang.emulator["sensor_enable"].c_str(), &emuenv.cfg.tiltsens);
         SetTooltipEx(lang.emulator["sensors_description"].c_str());
         
         if (!emuenv.cfg.tiltsens){
@@ -1142,7 +1142,7 @@ void draw_settings_dialog(GuiState &gui, EmuEnvState &emuenv) {
             ImGui::RadioButton("-90 degrees", &emuenv.cfg.tiltpos, -1);
             config::serialize_config(emuenv.cfg, emuenv.cfg.config_path);
         }else{
-            if (ImGui::Checkbox(lang.emulator["invert_gyro"].c_str(), &emuenv.cfg.invert_gyro)){}
+            ImGui::Checkbox(lang.emulator["invert_gyro"].c_str(), &emuenv.cfg.invert_gyro);
             SetTooltipEx(lang.emulator["invert_gyro_description"].c_str());
         }
 
