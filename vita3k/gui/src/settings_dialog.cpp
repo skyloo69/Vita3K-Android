e9e27e391e48e27ab2845955fed0ec8240e84efe
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-07-25 03:31:56 +0800

    Update settings_dialog.cpp

--- vita3k/gui/src/settings_dialog.cpp
+++ vita3k/gui/src/settings_dialog.cpp
@@ -950,6 +950,27 @@ void draw_settings_dialog(GuiState &gui, EmuEnvState &emuenv) {
                 ImGui::SetTooltip("%s", lang.gpu["mapping_method_description"].c_str());
             }
 
+            std::vector<const char *> vk_surface_format_strings = {
+                "Immediate",
+                "Mailbox",
+                "Fifo relaxed",
+                "Fifo"
+            };
+            std::vector<std::string_view> vk_surface_format_methods_indexes = {
+                "Immediate",
+                "mailbox",
+                "fifo-relaxed",
+                "fifo"
+            };
+
+            static int current_surface_format = std::find(surface_format_methods_indexes.begin(), surface_format_methods_indexes.end(), config.vk_mapping) - surface_format_methods_indexes.begin();
+            if (ImGui::Combo(lang.gpu["surface_format_method"].c_str(), &current_surface_format, surface_format_methods_strings.data(), surface_format_methods_strings.size())) {
+                config.vk_mapping = surface_format_methods_indexes[current_surface_format];
+            }
+            if (ImGui::IsItemHovered()) {
+                ImGui::SetTooltip("%s", lang.gpu[surface_format_method_description"].c_str());
+            }
+            
             if (is_ingame)
                 ImGui::EndDisabled();
         }
