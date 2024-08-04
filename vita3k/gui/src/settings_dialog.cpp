4c9a1b288a7af045911745058a3b80b94f5990c9
CrmC <79812109+ikhoeyZX@users.noreply.github.com>
2024-08-04 21:09:26 +0800

    Update settings_dialog.cpp

--- vita3k/gui/src/settings_dialog.cpp
+++ vita3k/gui/src/settings_dialog.cpp
@@ -1038,6 +1038,9 @@ void draw_settings_dialog(GuiState &gui, EmuEnvState &emuenv) {
         ImGui::Checkbox(lang.audio["enable_ngs_support"].c_str(), &config.ngs_enable);
         SetTooltipEx(lang.audio["ngs_description"].c_str());
         ImGui::Spacing();
+        ImGui::Checkbox(lang.audio["use_opensl_audio"].c_str(), &config.use_opensles);
+        SetTooltipEx(lang.audio["opensl_audio_description"].c_str());
+        ImGui::Spacing();
         ImGui::Separator();
         ImGui::Spacing();
         ImGui::EndTabItem();
